22a67830d7335485b51a772da2d2a4056562c682
Javi <javi.richards.a@gmail.com>
2022-07-16 12:48:38 -0400

    Agregado ruta de agregar productos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import AddProduct from './pages/AddProduct';
 import Login from './pages/Login'
 import Products from './pages/Products';
 import Sales from './pages/Sales'
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/" element={<Login />}></Route>
         <Route path="/sales" element={<Sales />}></Route>
         <Route path="/products" element={<Products />}></Route>
+        <Route path="/products/add" element={<AddProduct />}></Route>
       </Routes>
     </div>
   );
