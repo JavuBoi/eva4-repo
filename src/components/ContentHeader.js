b685db3529c3370db7a7ad1cdedf79cdd8bd0138
Javi <javi.richards.a@gmail.com>
2022-07-16 12:50:47 -0400

    ContentHeader: 2 Dynamic 2 Complex

--- src/components/ContentHeader.js
+++ src/components/ContentHeader.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function ContentHeader({ name }) {
+function ContentHeader({ title, name, parentName, parentRoute }) {
     return (
         <section className='content-header'>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-6">
-                        <h1>Panel de Ventas</h1>
+                        <h1>{title}</h1>
                     </div>
                     <div className="col-6">
                         <ol className="breadcrumb float-sm-right">
                             <li className="breadcrumb-item"><Link to="/sales">Cloud Sales</Link></li>
-                            <li className="breadcrumb-item active"> {name}</li>
+                            {parentName && <li className="breadcrumb-item"><Link to={parentRoute}>{parentName}</Link></li>}
+                            <li className={"breadcrumb-item " + (!parentName && "active")}> {name}</li>
                         </ol>
                     </div>
                 </div>
