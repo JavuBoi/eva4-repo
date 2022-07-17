905aac0d222c70de86036ca9d9bc7285519073c6
Javi <javi.richards.a@gmail.com>
2022-07-17 16:56:56 -0400

    Implementada opcion de agregar clases

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const InputGroup = ({ type, value, name, placeholder, readOnly, inputLabel, icon, setValue }) => {
+const InputGroup = ({ type, value, name, placeholder, readOnly, inputLabel, icon, setValue, style }) => {
     return (
 
         <div className={(inputLabel ? "form" : "input") + "-group mb-3"}>
@@ -8,14 +8,14 @@ const InputGroup = ({ type, value, name, placeholder, readOnly, inputLabel, icon
                 <label className="control-label" htmlFor={name}>{inputLabel}</label>
             )}
             <input
-                className="form-control"
+                className={"form-control" + (style ? style : "")}
                 type={type}
                 name={name}
                 id={name}
                 placeholder={placeholder}
                 readOnly={readOnly && "readonly"}
                 value={value}
-                onChange={(e) => setValue(e.target.value)} />
+                onChange={(e) => { setValue(e.target.value) }} />
             {icon && (
                 <div className="input-group-append">
                     <div className="input-group-text">
