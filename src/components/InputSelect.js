65947d2cc953bbcbe57100893f9e6906f9fe8f7d
Javi <javi.richards.a@gmail.com>
2022-07-17 16:57:55 -0400

    Ahora devuelve id de la opcion seleccionada

--- src/components/InputSelect.js
+++ src/components/InputSelect.js
@@ -5,15 +5,16 @@ class InputSelect extends React.Component {
         super(props);
         this.state = {
             options: [],
+            value: this.props.value,
+            setValue: this.props.setValue,
             name: this.props.name,
             inputLabel: this.props.inputLabel
         }
     }
 
     componentDidMount() {
-        const options = this.props.options ? this.props.options : [];
         this.setState({
-            options: options
+            options: this.props.optionList ? this.props.optionList : []
         })
     }
 
@@ -21,11 +22,13 @@ class InputSelect extends React.Component {
         const inputLabel = this.state.inputLabel;
         const name = this.state.name;
         const optionList = this.state.options;
+        const value = this.props.value
+        const setValue = this.props.setValue
 
         return (
             <div className="form-group mb-3">
                 <label className="control-label" htmlFor={name}>{inputLabel}</label>
-                <select className="form-control" name={name}>
+                <select className="form-control" name={name} value={value} onChange={(e) => setValue(e.target.value)} >
                     <option defaultValue={true} value="0">-- Seleccione</option>
                     {optionList.map(({ id, name }) => {
                         return (
